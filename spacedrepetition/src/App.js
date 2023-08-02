d98f97f9242ad025c28691ef8c5894cbf7524d91
Jeremy Krans <49561917+Jkrans@users.noreply.github.com>
2023-08-02 14:46:57 -0600

    removed some variables that were still causing errors

--- spacedrepetition/src/App.js
+++ spacedrepetition/src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import './index.css';
 import Header from './Components/Header'
-// import Button from './Components/Button'
-// import TextInput from './Components/TextInput'
 import DeckList from './Components/DeckList';
 
 function App() {
